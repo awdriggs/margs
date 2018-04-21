adcaea849ca46cc67728a3cb409d37f343f1af42
adam driggers <awdriggs@gmail.com>
2018-04-21 01:11:18 -0400

    basic styles

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    margs: 1,
-    tequila: {
-      scale: 1.5,
+      el: '#app',
+      data: {
+        margs: 0,
+        ingredients: [{
+            name: 'tequila',
+            scale: 1.5,
+            display: ""
+          }, {
+            name: 'cointreau',
+            scale: 1,
+            display: ""
+          }, {
+            name: 'lime',
+            scale: 0.75,
+            display: ""
+          }]
+          //computed properties for the actual drinks
+      },
+      methods: {
+        update: function() {
+            for (let item of this.ingredients) {
+              var total = this.margs * item.scale
+                //console.log(item.name, total);
+              if (total >= 8) {
+                var cups = total / 8;
+                var whole = Math.trunc(cups);
+                var part = Fraction(cups % 1);
+                item.display = `${whole ? `${whole}` : ``} ${whole && part.n ? ` and` : ``} ${part.n ? ` ${part.n}/${part.d}` : `` } ${cups > 1 ? `cups`:`cup`}`;
+           } else {
+              var whole = Math.trunc(total);
+              var part = Fraction(total % 1);
+              item.display = `${whole ? `${whole}` : ``} ${whole && part.n ? ` and` : ``} ${part.n ? ` ${part.n}/${part.d}` : `` } ${total > 1 ? `ounces`:`ounce`}`;
+          }
+        }
+      } //end of update 
     },
-    cointreau: {
-      scale: 1
-    },
-    lime: {
-      scale: 0.75
-    },
-
-//computed properties for the actual drinks
-  },
-  methods: {
-    update: function(){
-      console.log(this.margs);
-    }
-  }
 })
-
- 
